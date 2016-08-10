f09a52f273f07a43d09643f9161c0140cc038ae3
songhuiTan <276854396@qq.com>
2016-08-10 14:58:36 +0800

    changes log

---
+++ src/main/java/com/s/NewS.java
@@ -0,0 +1,43 @@
+package com.s;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NewS
+ */
+public class NewS extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NewS() {
+        super();
+      
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.print("========");
+		PrintWriter p=response.getWriter();
+		p.write("hasdasdaasdasdsdasdasdasads");
+		p.flush();
+		p.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
